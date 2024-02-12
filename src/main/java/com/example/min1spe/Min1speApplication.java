c8588b9b28520573573983fd518682e08c5777c0
salonisoniii <saloni.maheshwari@iiitb.ac.in>
2024-02-12 16:09:56 +0530

    initialCommit

---
+++ src/main/java/com/example/min1spe/Min1speApplication.java
@@ -0,0 +1,13 @@
+package com.example.min1spe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Min1speApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Min1speApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/min1spe/Min1speApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.min1spe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Min1speApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
