4c99f529bb142c99908bd3ff02badcc647ea3e93
salonisoniii <saloni.maheshwari@iiitb.ac.in>
2024-02-12 16:12:03 +0530

    secnd commit

--- src/main/java/com/example/min1spe/Min1speApplication.java
+++ src/main/java/com/example/min1spe/Min1speApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Scanner;
 
+
 @SpringBootApplication
 public class Min1speApplication {
 
